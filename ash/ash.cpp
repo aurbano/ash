d6e8885364388bb16231e6df1230d73f5a069cfc
Alejandro U. Alvarez <aluralma@gmail.com>
2015-08-21 11:57:04 -0400

    Added process forking, find in path, working dir

--- ash/ash.cpp
+++ ash/ash.cpp
@@ -2,7 +2,9 @@
 #include <sstream>
 #include <vector>
 #include <stdio.h>
+#include <fcntl.h>
 #include <sys/types.h>
+#include <stdlib.h>
 #ifdef WINDOWS
     #include <direct.h>
     #define GetCurrentDir _getcwd
@@ -11,30 +13,29 @@
     #define GetCurrentDir getcwd
 #endif
 
-char PWD[FILENAME_MAX];
+char PWD[FILENAME_MAX];     // Current working directory
+char *PATH;                 // PATH environmental variable
+char **PATHDIRS = NULL;     // Each path in the PATH var
 
-int executeCmd(char cmd[], char **argv, int in, int out){
-    // Fork a new process for this
-    pid_t kidpid = fork();
-    
-    if ( kidpid < 0 ){
-        perror( "Internal error: cannot fork." );
-        exit(-1);
-    }else if ( kidpid == 0 ){
-        // I am the child.
-        execvp( cmd, argv );
-        // The following lines should not happen (normally).
-        perror( cmd );
-        exit(-1);
-    }else{
-        // I am the parent.  Wait for the child.
-        if ( waitpid( kidpid, 0, 0 ) < 0 ){
-            perror( "Internal error: cannot wait for child." );
-            exit(-1);
-        }
-        // All done
-        return 0;
+void updatePath();
+void parsePathDirs();
+bool isExecutable(char *, char **);
+int executeCmd(char[], char **, int, int);
+void waitForInput();
+
+int main(int argc, const char * argv[]) {
+    // Get the working directory
+    if (!GetCurrentDir(PWD, sizeof(PWD))){
+        return errno;
     }
+    PWD[sizeof(PWD) - 1] = '\0';
+    
+    std::cout << "Welcome to the ash shell!\n";
+    
+    while (1) {
+        waitForInput();
+    }
+    return 0;
 }
 
 void waitForInput(){
@@ -55,8 +56,7 @@ void waitForInput(){
     std::vector<char*> args;
     char* prog = strtok( cmd, " " );
     char* tmp = prog;
-    while ( tmp != NULL )
-    {
+    while ( tmp != NULL ){
         args.push_back( tmp );
         tmp = strtok( NULL, " " );
     }
@@ -71,23 +71,121 @@ void waitForInput(){
     
     if(executeCmd(cmd, argv, handles[0], handles[1]) >= 0){
         // All done
-    }else{
-        std::cerr << "An error occured running the cmd";
     }
 }
 
-int main(int argc, const char * argv[]) {
-    // Get the working directory
-    if (!GetCurrentDir(PWD, sizeof(PWD))){
-        return errno;
-    }
-    PWD[sizeof(PWD) - 1] = '\0';
+int executeCmd(char cmd[], char **argv, int in, int out){
+    // Fork a new process for this
+    pid_t kidpid = fork();
     
-    std::cout << "Welcome to the ash shell!\n";
-    
-    while (1) {
-        waitForInput();
+    if ( kidpid < 0 ){ // Error
+        perror( "Internal error: cannot fork." );
+        return -1;
+    }else if ( kidpid == 0 ){ // Child
+        // Redirect output
+        //int fd = open("/dev/ttyS0", O_WRONLY);
+        //dup2(fd, out);  // redirect stdout
+        
+        char *pathCmd;
+        if(isExecutable(cmd, &pathCmd) == 0){
+            std::cout << "Executing " << pathCmd << std::endl;
+            execv(pathCmd, argv);
+            return 0;
+            
+        }else{
+            std::cerr << "ash: command not found: " << cmd << std::endl;
+            return -1;
+        }
+    }else{ // Parent
+        int kidStatus;
+        pid_t waitp = waitpid(kidpid, &kidStatus,0); // 0 means blocking
+        // I am the parent.  Wait for the child.
+        if (waitp < 0 ){
+            std::cerr <<  "Internal error: wait for child failed [status = " << kidStatus << "]" << std::endl;
+            return -1;
+        }
+        std::cout << "Parent: child exited with status: " << kidStatus << std::endl;
+        // All done
+        return 0;
     }
-    return 0;
+}
+
+// Return true if executable, while also writing the full path in path
+bool isExecutable(char *cmd, char **path){
+    // Check if path contains / (absolute/relative cmd)
+    bool inDir = 0;
+    
+    int i = 0;
+    while(cmd[i]) {
+        if(cmd[i] == '/') {
+            inDir = true;
+        }
+        i++;
+    }
+    
+    if(inDir){
+        std::cout << "Checking inDir cmd: " << cmd << std::endl;
+        // Search for command directly
+        if(access(cmd, X_OK) == 0) {
+            *path = cmd;
+            return 0;
+        }else{
+            return -1;
+        }
+    }else{
+        // Search for command in path
+        // Reload the path, in case it changed
+        parsePathDirs();
+        
+        for(i = 0; PATHDIRS[i]; i++) {
+            // build path
+            char *tmpPath = (char *) malloc(strlen(PATHDIRS[i])+1+strlen(cmd));
+            strcpy(tmpPath, PATHDIRS[i]);
+            strcat(tmpPath, "/");
+            strcat(tmpPath, cmd);
+            
+            if(access(tmpPath, X_OK) == 0) {
+                *path = tmpPath;
+                return 0;
+            }
+        }
+        
+        return -1;
+    }
+}
+
+void updatePath(){
+    // Update the size of path
+    PATH = (char *) malloc((strlen(getenv("PATH")) + 1) * sizeof(char));
+    strcpy(PATH, getenv("PATH"));
+}
+
+void parsePathDirs() {
+    updatePath();
+    
+    int i = 1, j = 0;
+    
+    // Calculate number of dirs in PATH
+    while(PATH[j]) {
+        if(PATH[j] == ':'){
+            i++;
+        }
+        j++;
+    }
+    
+    // Determine the size of PATHDIRS
+    free(PATHDIRS);
+    PATHDIRS = (char **) malloc((i + 1) * sizeof(char *));
+    PATHDIRS[0] = PATH;
+    PATHDIRS[i] = 0;
+    
+    // Change the : for null chars and save it into PATHDIRS
+    for(i = 1, j = 0; PATH[j]; j++) {
+        if(PATH[j] == ':') {
+            PATH[j] = '\0';
+            PATHDIRS[i] = &PATH[j+1];
+            i++;
+        }
+    }
 }
 
