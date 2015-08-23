8a4061194d1d8c390f2ee73eaea7b6ef07dedb4b
Alejandro U. Alvarez <aluralma@gmail.com>
2015-08-22 20:12:20 -0400

    CD builtin working :)

--- src/ash.cpp
+++ src/ash.cpp
@@ -8,11 +8,11 @@
 #include <cstdio>
 #include "builtins.h"
 #ifdef WINDOWS
-    #include <direct.h>
-    #define GetCurrentDir _getcwd
+#include <direct.h>
+#define GetCurrentDir _getcwd
 #else
-    #include <unistd.h>
-    #define GetCurrentDir getcwd
+#include <unistd.h>
+#define GetCurrentDir getcwd
 #endif
 
 char PWD[FILENAME_MAX];
@@ -20,6 +20,7 @@ const int BUFFERSIZE = 1024;// Read buffer size
 char *PATH;                 // PATH environmental variable
 char **PATHDIRS = NULL;     // Each path in the PATH var
 
+char* getWorkingDir();
 void updatePath();
 void parsePathDirs();
 bool isExecutable(char *, char **);
@@ -28,34 +29,38 @@ int executeCmd(char[], char **, int, int);
 void waitForInput();
 
 int main(int argc, const char * argv[]) {
-    // Get the working directory
-    if (!GetCurrentDir(PWD, sizeof(PWD))){
-        return errno;
-    }
-    PWD[sizeof(PWD) - 1] = '\0';
-    
-    std::cout << "Welcome to the ash shell!\n";
-    
+
+    std::cout << "Welcome to the ash shell!!\n";
+
     while (1) {
         waitForInput();
     }
     return 0;
 }
 
+char* getWorkingDir(){
+    // Get the working directory
+    if (!GetCurrentDir(PWD, sizeof(PWD))){
+        return 0;
+    }
+    PWD[sizeof(PWD) - 1] = '\0';
+    return PWD;  
+}
+
 void waitForInput(){
-    std::cout << PWD << " $ ";
-    
+    std::cout << getWorkingDir() << " $ ";
+
     // Input buffer
     char cmd[BUFFERSIZE];
-    
+
     // Read the command
     std::cin.getline(cmd, BUFFERSIZE);
-    
+
     // IO, default to stdin, stdout
     int handles[2];
     handles[0] = 0;
     handles[1] = 1;
-    
+
     // Get command and arguments
     std::vector<char*> args;
     // Split by space
@@ -66,19 +71,16 @@ void waitForInput(){
         args.push_back( tmp );
         tmp = strtok( NULL, " " );
     }
-    
+
     // Build argv
     char** argv = new char*[args.size()+1];
     for ( int k = 0; k < args.size(); k++ ){
         argv[k] = args[k];
     }
-    
+
     argv[args.size()] = NULL;
-    
-    if(executeCmd(cmd, argv, handles[0], handles[1]) >= 0){
-        // All done
-        std::cout << "cmd executed" << std::endl;
-    }else{
+
+    if(executeCmd(cmd, argv, handles[0], handles[1]) < 0){
         // Execution failed
         std::cout << "ash: failed to execute" << std::endl;
         perror("ash: failed to execute command");
@@ -86,9 +88,19 @@ void waitForInput(){
 }
 
 int executeCmd(char cmd[], char **argv, int in, int out){
-    // Fork a new process for this
+    // Check if its a builtin
+    if(builtins::fn cmdFn = isBuiltin(cmd)){
+        int args = sizeof(argv)/sizeof(*argv)+1;
+        int res = (*cmdFn)(args, argv);
+        if(res < 0){
+            std::cerr << "ash: command failed: " << cmd << std::endl;
+        }
+        return res;
+    }
+
+    // Not a builtin, fork a process for it
     pid_t kidpid = fork();
-    
+
     if ( kidpid < 0 ){ // Error
         perror( "ash: internal error: cannot fork child process." );
         return -1;
@@ -96,26 +108,20 @@ int executeCmd(char cmd[], char **argv, int in, int out){
         // Redirect output
         //int fd = open("/dev/ttyS0", O_WRONLY);
         //dup2(fd, out);  // redirect stdout
-        
+
         char *pathCmd;
-        if(builtins::fn cmdFn = isBuiltin(cmd)){
-            int args = sizeof(argv)/sizeof(*argv)+1;
-            std::cout << "Running builtin with argc=" << args << std::endl;
-            int res = (*cmdFn)(args, argv);
-            if(res < 0){
-                std::cerr << "ash: command failed: " << cmd << std::endl;
-            }
-            return res;
-        }else if(isExecutable(cmd, &pathCmd) == 0){
+        if(isExecutable(cmd, &pathCmd) == 0){
             execv(pathCmd, argv);
-            return 0;
+            exit(0);
         }else{
             std::cerr << "ash: command not found: " << cmd << std::endl;
-            return -1;
+            exit(-1);
         }
     }else{ // Parent
         int kidStatus;
-        waitpid(kidpid, &kidStatus,0); // 0 means blocking
+        int wpid;
+        // Wait for all children (useful is command creates children itself)
+        while ((wpid = wait(&kidStatus)) > 0);
         // I am the parent.  Wait for the child.
         if (kidStatus < 0 ){
             std::cerr <<  "ash: last command errored (status " << kidStatus << ")" << std::endl;
@@ -128,7 +134,6 @@ int executeCmd(char cmd[], char **argv, int in, int out){
 
 builtins::fn isBuiltin(char *cmd){
     if(strcmp(cmd, "cd")==0){
-        std::cout << "Builtin: cd" << std::endl;
         return builtins::cd;
     }
     return 0;
@@ -138,7 +143,7 @@ builtins::fn isBuiltin(char *cmd){
 bool isExecutable(char *cmd, char **path){
     // Check if path contains / (absolute/relative cmd)
     bool inDir = 0;
-    
+
     int i = 0;
     while(cmd[i]) {
         if(cmd[i] == '/') {
@@ -146,9 +151,8 @@ bool isExecutable(char *cmd, char **path){
         }
         i++;
     }
-    
+
     if(inDir){
-        std::cout << "Checking inDir cmd: " << cmd << std::endl;
         // Search for command directly
         if(access(cmd, X_OK) == 0) {
             *path = cmd;
@@ -160,20 +164,20 @@ bool isExecutable(char *cmd, char **path){
         // Search for command in path
         // Reload the path, in case it changed
         parsePathDirs();
-        
+
         for(i = 0; PATHDIRS[i]; i++) {
             // build path
             char *tmpPath = (char *) malloc(strlen(PATHDIRS[i])+1+strlen(cmd));
             strcpy(tmpPath, PATHDIRS[i]);
             strcat(tmpPath, "/");
             strcat(tmpPath, cmd);
-            
+
             if(access(tmpPath, X_OK) == 0) {
                 *path = tmpPath;
                 return 0;
             }
         }
-        
+
         return -1;
     }
 }
@@ -186,9 +190,9 @@ void updatePath(){
 
 void parsePathDirs() {
     updatePath();
-    
+
     int i = 1, j = 0;
-    
+
     // Calculate number of dirs in PATH
     while(PATH[j]) {
         if(PATH[j] == ':'){
@@ -196,13 +200,13 @@ void parsePathDirs() {
         }
         j++;
     }
-    
+
     // Determine the size of PATHDIRS
     free(PATHDIRS);
     PATHDIRS = (char **) malloc((i + 1) * sizeof(char *));
     PATHDIRS[0] = PATH;
     PATHDIRS[i] = 0;
-    
+
     // Change the : for null chars and save it into PATHDIRS
     for(i = 1, j = 0; PATH[j]; j++) {
         if(PATH[j] == ':') {
--- src/builtins/cd.cpp
+++ src/builtins/cd.cpp
@@ -6,37 +6,27 @@
 #include <cstring>
 #include "builtins.h"
 
-extern char* PWD;
-
 namespace builtins{
     int cd(int argc, char *argv[]) {
         int res;
 
-        std::cout << "ASH: CD BUILTIN, changing dir to " << argv[1] << ", num args="<< argc << std::endl;
-
         // If cd has no arguments go to the home dir
         // else go to the directort in the argument
         if(argc == 1) {
-            std::cout << "Changed dir to " << PWD << std::endl;
             res = chdir(getenv("HOME"));
-            strcpy(PWD, getenv("HOME"));
         }else if (argc == 2) {
-            std::cout << "Changed dir to " << PWD << std::endl;
             res = chdir(argv[1]);
-            strcpy(PWD, argv[1]);
         } else {
+            std::cout << "Incorrect usage" << std::endl;
             // Display error if more than one arg
             printf("ash: incorrect usage\n Usage: cd [directory]");
             return(6);
         }
-        std::cout << "before last switchndef" << std::endl;
 
         //if chdir doesn't fail exit. Else report the error
         if(res != -1) {
-            std::cout << "done" << std::endl;
             return(0);      
         }else {
-            std::cout << "CD FAILED" << std::endl;
             perror("ash: internal error ");
             return(-2);
         }
